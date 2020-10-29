7bc6446cc86c89ef48732a1432e45b7d7573b48e
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-10-29 18:04:48 -0400

    adding route files

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+//write this in individual folders for routes (hotrestaurant)
+//require("./routes/api-routes")(app);
+//require("./routes/html-routes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
